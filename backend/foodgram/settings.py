41bcf622bc0048ba91e678c0206ad9865333df6b
Potapov Artem <Tmnt99@yandex.ru>
2023-10-19 12:49:27 +0300

    Update recipes models.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
     'api.apps.ApiConfig',
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -124,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -5,35 +5,80 @@ from users.models import User
 
 class Tag():
     """Класс для тегов."""
-    pass
+    name = models.CharField(
+        verbose_name='Название'
+    )
+    color = models.CharField(
+        verbose_name='Цвет в HEX',
+        max_length=16
+    )
+    slug = models.SlugField(
+        verbose_name='Уникальный слаг'
+        # Сделать валидацию
+    )
 
 
 class Ingridient():
     """Класс для ингридиентов."""
-    pass
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единицы измерения'
+    )
 
 
 class Recipe(models.Model):
     """Класс для рецептов."""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='recipes'
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='recipes'
     )
     name = models.CharField(
+        verbose_name='Название',
         max_length=200
     )
     image = models.ImageField(
-        upload_to=
+        verbose_name='Картинка, закодированная в Base64',
+        upload_to='recipe_images/', null=True, blank=True
     )
     text = models.CharField(
-
+        verbose_name='Описание',
     )
     ingridients = models.ManyToManyField(
         Ingridient,
-
+        verbose_name='Список ингредиентов',
+        related_name='recipes',
+        through='RecipeIngridients'
     )
     tags = models.ManyToManyField(
         Tag,
+        verbose_name='Список id тегов',
+        related_name='recipes'
 
     )
-    cooking_time = 
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления (в минутах)'
+        # Сделать валидатор на миним значение
+    )
 
+
+class RecipeIngridient(models.Model):
+    recipes = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепты',
+        on_delete=models.CASCADE
+    )
+    ingridients = models.ForeignKey(
+        Ingridient,
+        verbose_name='Ингридиенты',
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество'
+        # Добавить валидацию не меньше 1
+    )
