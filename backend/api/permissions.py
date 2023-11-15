7603b0276a8663fadc550687a36dca41e563e940
Артем Потапов <tmmt99@yandex.ru>
2023-11-15 22:40:11 +0300

    Fix bugs.

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,4 +5,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """Доступ только для автора, либо только чтение."""
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user == obj.author
+        return request.user.is_authenticated and request.user.recipes == obj.author
